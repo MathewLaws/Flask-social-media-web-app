b44625f708c69d18e04f02ee33f5a2b788571109
Mathewlaws1 <93098734+Mathewlaws1@users.noreply.github.com>
2021-12-19 18:19:08 +0000

    Add files via upload

---
+++ flask_socialmedia_app/index.py
@@ -0,0 +1,8 @@
+from re import I
+from flask import Flask, render_template, request
+from website import *
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True, host="192.168.0.193")
